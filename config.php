f94df4e4fbb7cb0621ace5c526ec48c88a0b239c
raam2 <100071712+raam2@users.noreply.github.com>
2025-10-09 22:00:34 +0530

    Update  config.php

--- config.php
+++ config.php
@@ -8,17 +8,17 @@ $config = [
     'local' => [
         'host' => getenv('DB_HOST') ?: 'localhost',
         'port' => getenv('DB_PORT') ?: '3306',
-        'user' => getenv('DB_USER') ?: 'u184420243_gst4',
+        'user' => getenv('DB_USER') ?: 'xxxxxxxx',
         'pass' => getenv('DB_PASS') ?: 'Raam2*:1',
-        'db'   => getenv('DB_NAME') ?: 'u184420243_jayanti_enter4',
+        'db'   => getenv('DB_NAME') ?: 'xxxxxxxxxxx',
         'charset' => 'utf8mb4',
     ],
     'production' => [
-        'host' => getenv('DB_HOST') ?: '217.21.95.103',
+        'host' => getenv('DB_HOST') ?: '217.xx.xx.103',
         'port' => getenv('DB_PORT') ?: '3306',
-        'user' => getenv('DB_USER') ?: 'u184420243_gst4',
-        'pass' => getenv('DB_PASS') ?: 'Raam2:=195',
-        'db'   => getenv('DB_NAME') ?: 'u184420243_jayanti_enter4',
+        'user' => getenv('DB_USER') ?: 'xxxxxx',
+        'pass' => getenv('DB_PASS') ?: 'xxxxxxxxxx',
+        'db'   => getenv('DB_NAME') ?: 'xxxxxxxxx',
         'charset' => 'utf8mb4',
     ],
 ];
